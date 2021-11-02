d26b1802cd4dce3351c458087d50abb33b8f04db
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-02 14:53:13 +0100

    modif

--- exercice3.js
+++ exercice3.js
@@ -1,4 +1,4 @@
 let name = "Clement"
 let age = "25"
 let city = "Versailles"
-alert("Bonjour, je m'appelle " + name + " j'ai " + age + " et j'habite " + city)
+alert("Bonjour, je m'appelle " + name + ", j'ai " + age + " et j'habite " + city + ".")
